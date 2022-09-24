49d6b89123bcbc8ca03f79391d47c78cf2acbca8
Allah007 <troevjulus041110@gmail.com>
2022-09-24 13:31:48 +0300

    added hello_man program

---
+++ 02_compiler/hello_man.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char var;
+
+    cout << "Введите фамилию: ";
+    cin >> var;
+
+    cout << "Hello, "<< var <<" !" << endl;
+
+    return 0;
+}
---
+++ 03_cpp/intro/hello_man.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string var;
+
+    cout << "Введите фамилию: ";
+    cin >> var;
+    
+    cout << "Hello, " << var << "!" << endl;
+}
