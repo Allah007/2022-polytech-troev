604ae096c45d8af258ada5feff8ad8d8d0820c26
Allah007 <troevjulus041110@gmail.com>
2022-09-24 15:00:17 +0300

    updated

---
+++ 03_cpp/intro/build/CMakeFiles/hello_men.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/troev/2022-polytech-troev/03_cpp/intro/hello_man.cpp" "CMakeFiles/hello_men.dir/hello_man.cpp.obj" "gcc" "CMakeFiles/hello_men.dir/hello_man.cpp.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ 03_cpp/intro/build/CMakeFiles/hello_men.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hello_men.dir/hello_man.cpp.obj"
+  "CMakeFiles/hello_men.dir/hello_man.cpp.obj.d"
+  "hello_men.exe"
+  "hello_men.exe.manifest"
+  "hello_men.pdb"
+  "libhello_men.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hello_men.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
