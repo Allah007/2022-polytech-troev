213362e852126c37bdea5eb870e16195ae831117
Allah007 <troevjulus041110@gmail.com>
2022-09-30 21:53:58 +0300

    added sorting program

---
+++ 04_search_sort/intro/lin_bin_sort.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int arr[] = {-996, -960, -944, -887, -865, -860, -846, -831, -790, -785, -700, -691, -662, -662, -647, 
+    -640, -587, -532, -524, -519, -501, -497, -475, -471, -461, -457, -452, -442, -441, -416, 
+    -412, -411, -390, -389, -358, -341, -306, -257, -253, -252, -248, -208, -143, -134, -82, 
+    -79, -11, 21, 26, 48, 68, 69, 72, 93, 95, 99, 115, 120, 156, 164, 181, 200, 231, 252, 288, 
+    304, 336, 343, 364, 402, 419, 486, 546, 584, 595, 596, 606, 627, 636, 666, 666, 683, 701, 
+    740, 766, 780, 782, 794, 802, 829, 832, 837, 858, 875, 881, 882, 883, 883, 947, 959};
+
+    int low = 0;
+
+    char sort;
+
+    int ArrayLen = sizeof(arr)/sizeof(int);
+
+    cout << "B or L?" << endl;
+
+    cin >> sort;
+
+    switch(sort) {
+
+        case 'L':
+
+            for (int i = 0; i < ArrayLen; i++){
+
+                if (arr[i] == 120){
+
+                    cout << "120 has been found by a Linear search method at the index of " << i << endl;
+
+                    break;
+
+                }
+
+            }
+
+            break;
+
+        case 'B':
+
+            int high = ArrayLen - 1;
+
+            while (low <= high){
+
+                int mid = (low + high) / 2;
+
+                if (arr[mid] == 120){
+
+                    cout << "120 has been found by a Binary search method at the index of " << mid << endl;
+
+                    break;
+
+                }
+
+                else if (120 > arr[mid]){
+
+                    low = mid + 1;
+
+                }
+                else {
+
+                    high = mid - 1;
+
+                }
+
+            }
+
+            break;
+
+    }
+
+    return 0;
+
+}
+
+
+
