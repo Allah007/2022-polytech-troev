6bf472421fd7cc66a4fc10290164e4b171464399
Allah007 <troevjulus041110@gmail.com>
2022-10-22 00:42:53 +0300

    added meminfo

---
+++ 06_pointers_references_tasks/palindrome.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+bool (){
+
+    string pal;
+
+    cin >> pal;
+
+    int l = pal.length
+
+    for (int i = 0; i != l; i++){
+
+        
+
+    }
+    
+}
