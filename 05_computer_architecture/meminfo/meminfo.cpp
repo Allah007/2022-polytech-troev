b5f50253984a180985423a787af9e924f9b6797a
Allah007 <troevjulus041110@gmail.com>
2022-12-01 19:59:34 +0300

    fixed meminfo code

--- 05_computer_architecture/meminfo/meminfo.cpp
+++ 05_computer_architecture/meminfo/meminfo.cpp
@@ -1,55 +1,31 @@
 #include <iostream>
 #include <fstream>
-#include <cmath>
 
-void show_meminfo(){
+int main()
+{
+	char parameter[20]; 
+	int value; 
+	char unit[3];   
+	double memory[2];
 
-    char parameter[20];
-
-    int value;
-
-    char unit[3];
-
-    int total, free, avail;
-
-    std::ifstream in("/proc/meminfo", std::ios_base::in);
-
-    if(in.is_open()){
-
-        in >> parameter >> total >> unit;
-
-        in >> parameter >> free >> unit;
-
-        in >> parameter >> avail >> unit;
-
-    } 
-    
-    else {
-
-        exit(2);
-
-    }
-
-    float percent = (total - avail) / static_cast<float>(total) * 100;
-
-    int bars_count = round(percent/10);
-
-    for (int i = 0; i != bars_count; ++i)
-
-        std::cout << "|";
-
-    for (int i = 0; i != 10 - bars_count; ++i)
-
-        std::cout << ".";
-
-    std::cout << " " << static_cast<int>(percent) << "%" << std::endl;
-    
-}
-
-int main(){
-
-    show_meminfo();
-
-    return 0;
+	std::ifstream in("/proc/meminfo", std::ios_base::in);
+	for(int i = 0; i < 2; i++) 
+	{
+		in >> parameter >> value >> unit;
+		memory[i] = value;
+		std::cout << memory[i] << std::endl;
+	}
+	double p = memory[1] / memory[0];
+	int percentage = p * 100;
+	for (int i = 0; i < percentage; i++)
+	{
+		std::cout << (char)124;
+	}
+	for (int i = 0; i < (100 - percentage); i++)
+	{
+		std::cout << (char)46;
+	}
+	std::cout << " " << percentage << "%" << std::endl;
+	return 0;
 
 }
